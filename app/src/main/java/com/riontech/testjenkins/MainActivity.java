a551125bafd248869130fd1c38d4b39a3358e815
Dhaval <dhaval@riontech.com>
2016-06-06 17:47:45 +0530

    Initial project commit.

---
+++ app/src/main/java/com/riontech/testjenkins/MainActivity.java
@@ -0,0 +1,16 @@
+package com.riontech.testjenkins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtView = (TextView) findViewById(R.id.txtJenkins);
+    }
+}
